ebfdef5f25f73b25c08504894769f0e823259127
Blueboyding <2495862996@qq.com>
2022-12-07 17:20:21 +0800

    55

--- src/Solutions.java
+++ src/Solutions.java
@@ -24,7 +24,7 @@
 //  }
 public class Solutions {
     public static void main(String[] args) {
-        int[] test = new int[]{3, 2, 4, 7, 5};
+        int[] test = new int[]{3, 2, 4, 7, 55};
 
         for (int i = 0; i < test.length; i++) {
             System.out.println(test[i]);

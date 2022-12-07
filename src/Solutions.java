813bb600f5231ff7bad275ca50bf0c06324bbd49
Blueboyding <2495862996@qq.com>
2022-12-07 17:08:30 +0800

    first commit

---
+++ src/Solutions.java
@@ -0,0 +1,36 @@
+/**
+ * @author shkstart
+ * @create 2022-11 13:21
+ */
+//class ListNode {
+//      int val;
+//      ListNode next;
+//      ListNode() {}
+//      ListNode(int val) { this.val = val; }
+//      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  }
+
+//      class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode() {}
+//      TreeNode(int val) { this.val = val; }
+//      TreeNode(int val, TreeNode left, TreeNode right) {
+//          this.val = val;
+//          this.left = left;
+//          this.right = right;
+//      }
+//  }
+public class Solutions {
+    public static void main(String[] args) {
+        int[] test = new int[]{3, 2, 4, 7, 5};
+
+        for (int i = 0; i < test.length; i++) {
+            System.out.println(test[i]);
+        }
+    }
+    public static void solution(){
+
+    }
+}
